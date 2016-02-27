4a40c185d1500112d5c9a6ebe78c923b770c241a
Jonathan Hinton <hintonjonathan@gmail.com>
2016-02-26 21:23:23 -0600

    set-up initial expression test

--- Calculator/Calculator.Tests/ExpressionTest.cs
+++ Calculator/Calculator.Tests/ExpressionTest.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
 
 namespace Calculator.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class ExpressionTest
     {
         [TestMethod]
         public void TestMethod1()
