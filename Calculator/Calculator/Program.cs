0dccb5de98252f63ee1340a9c8553d47bf807665
Jonathan Hinton <hintonjonathan@gmail.com>
2016-03-18 22:49:24 -0500

    increment counter, implement counter

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -13,6 +13,7 @@ namespace Calculator
             Evaluate my_eval = new Evaluate();
             Terminal user_term = new Terminal();
             Console.WriteLine("type 2 terms you would like me to do some math on. I can add, subtract, multiply, divide, modulo and save constants...maybe");
+            Console.WriteLine(user_term.PromptFormat());
             while (true)
             {
                 string user_input = Console.ReadLine();
@@ -22,14 +23,16 @@ namespace Calculator
                     Environment.Exit(0);
                 } else if (user_input == "last")
                 {
-
-                    Console.WriteLine("Your last answer was {0}", my_eval.LastOutput(user_input));
+                    user_term.Counter++;
+                    Console.WriteLine(user_term.PromptFormat() + " Your last answer was {0}", my_eval.LastOutput(user_input));
                 } else if (user_input == "lastq")
                 {
-                    Console.WriteLine("Your last question was {0}", my_eval.LastInput(user_input));
+                    user_term.Counter++;
+                    Console.WriteLine(user_term.PromptFormat() + " Your last question was {0}", my_eval.LastInput(user_input));
                 } else
                 {
-                    Console.WriteLine("the answer is {0}...I think", my_eval.handledIt(user_input));
+                    user_term.Counter++;
+                    Console.WriteLine(user_term.PromptFormat() + " the answer is {0}...I think", my_eval.handledIt(user_input));
                 }
 
             }
