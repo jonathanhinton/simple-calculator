6823adf20a0352c3f3cebc82d996ba8905112244
Jonathan Hinton <hintonjonathan@gmail.com>
2016-03-18 22:41:03 -0500

    UI working but very unstable

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -10,6 +10,29 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Evaluate my_eval = new Evaluate();
+            Terminal user_term = new Terminal();
+            Console.WriteLine("type 2 terms you would like me to do some math on. I can add, subtract, multiply, divide, modulo and save constants...maybe");
+            while (true)
+            {
+                string user_input = Console.ReadLine();
+                if (user_input == "exit" || user_input == "quit")
+                {
+                    Console.WriteLine("bye");
+                    Environment.Exit(0);
+                } else if (user_input == "last")
+                {
+
+                    Console.WriteLine("Your last answer was {0}", my_eval.LastOutput(user_input));
+                } else if (user_input == "lastq")
+                {
+                    Console.WriteLine("Your last question was {0}", my_eval.LastInput(user_input));
+                } else
+                {
+                    Console.WriteLine("the answer is {0}...I think", my_eval.handledIt(user_input));
+                }
+
+            }
         }
     }
 }
---
+++ Calculator/Calculator/Terminal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Terminal
+    {
+        public int Counter { get; set; }
+        public Terminal()
+        {
+            Counter = 0;
+        }
+
+        public string PromptFormat()
+        {
+            return String.Format("[{0}]>", Counter);
+        }
+    }
+}
